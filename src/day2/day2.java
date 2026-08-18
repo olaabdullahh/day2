85c4736e6fb0a7f2f8d484f5a9d191890bcd65f9
ola <alaalabdallua@gmail.com>
2026-08-18 20:59:02 +0300

    first commit day two

---
+++ src/day2/day2.java
@@ -0,0 +1,100 @@
+package day2;
+
+import java.util.Scanner;
+
+public class day2 {
+    static void main() {
+        // example one
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("enter number");
+//       int number = scanner.nextInt();
+//
+//       if(number%2==0){
+//           System.out.println("even");
+//       }else{
+//           System.out.println("Odd");
+//       }
+//
+//        scanner.close();
+//
+
+       // example two
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("enter number");
+//        int number = scanner.nextInt();
+//
+//       if(number>0){
+//           System.out.println("positive");
+//       }else if(number<0){
+//           System.out.println("negative");
+//       }else{
+//           System.out.println("Zero");
+//       }
+//
+//        scanner.close();
+
+
+        // example three
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("enter first number");
+//        int number1 = scanner.nextInt();
+//
+//        System.out.println("enter second number");
+//        int number2 = scanner.nextInt();
+//
+//        System.out.println("enter third number");
+//        int number3 = scanner.nextInt();
+//
+//        if(number1> number2 && number1 > number3){
+//            System.out.println("number one is largest");
+//        }else if(number2>number1 &&number2>number3)
+//            System.out.println("number two is largest");
+//        else
+//            System.out.println("third number is largest");
+
+//scanner.close();
+
+
+        // example four
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("enter first number");
+//        int numberOfDay = scanner.nextInt();
+//        if(numberOfDay == 1)
+//            System.out.println("saturday");
+//        else if(numberOfDay == 2)
+//            System.out.println("sunday");
+//        else if(numberOfDay == 3)
+//            System.out.println("monday");
+//        else if(numberOfDay == 4)
+//            System.out.println("tuesday");
+//        else  if(numberOfDay == 5)
+//            System.out.println("wednesday");
+//        else   if(numberOfDay == 6)
+//            System.out.println("thursday");
+//        else if(numberOfDay == 7)
+//            System.out.println("friday");
+//        else System.out.println("valid day");
+
+//        scanner.close();
+
+//        switch (numberOfDay) {
+//            case 1: System.out.println("saturday");break;
+//            case 2: System.out.println("sunday");break;
+//            case 3: System.out.println("monday");break;
+//            case 4: System.out.println("tuesday");break;
+//            case 5: System.out.println("wednesday");break;
+//            case 6: System.out.println("thursday");break;
+//            case 7: System.out.println("friday");break;
+//            default: System.out.println("valid day");
+//
+//scanner.close();
+
+//        }
+
+
+    }
+
+
+
+}
---
+++ src/day2/loops.java
@@ -0,0 +1,16 @@
+package day2;
+
+import java.util.Scanner;
+
+public class loops {
+    static void main() {
+        int rate = 10;
+        int maxWeeklyHours=40;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter Working Hours");
+        double WorkingHours = scanner.nextDouble();
+
+
+
+    }
+}
